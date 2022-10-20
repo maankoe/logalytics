97ea7a6e6da4e4452a3ec288eae9625327eb48d7
maanrit <108694058+maanrit@users.noreply.github.com>
2022-10-20 21:17:40 +0200

    Updated sim and generated log data

--- sim/src/primes/main.py
+++ sim/src/primes/main.py
@@ -1,9 +1,11 @@
 import logging
 from pathlib import Path
 
+import random
+
 log_file = Path(__file__).parent.parent.parent / "data" / "primes.log"
 
-log_format = "%(asctime)s:%(message)s"
+log_format = "%(asctime)s: %(message)s"
 logging.basicConfig(filename=log_file, filemode="w", format=log_format)
 logger = logging.getLogger("primes")
 logger.setLevel(logging.DEBUG)
@@ -16,13 +18,19 @@ def is_prime(x):
     return True
 
 
-def gen_primes(max_val):
-    for x in range(2, max_val):
-        logger.info(x)
+def gen_primes(candidates):
+    for x in candidates:
+        logger.info("Testing: {0}".format(x))
         if is_prime(x):
             yield x
 
 
+def gen_candidates(n_candidates, max_val):
+    logger.info("Candidates: {0}, max val: {1} ".format(n_candidates, max_val))
+    return (random.randint(2, max_val) for _ in range(n_candidates))
+
+
 if __name__ == "__main__":
-    primes = gen_primes(100000)
-    print(list(primes))
+    for _ in range(5):
+        primes = gen_primes(gen_candidates(100, 1000000))
+        print(list(primes))
