31a1754ebf5b2efd8296e9d676d319c038039b79
maanrit <108694058+maanrit@users.noreply.github.com>
2022-10-23 20:23:03 +0200

    Loads log file into Entry objects

--- src/logalytics/engine/engine.py
+++ src/logalytics/engine/engine.py
@@ -1,7 +1,10 @@
 from logalytics.model.entry import Entry
-from logalytics.parsing.parse import parse_entry
+from logalytics.parsing.parse import parse_entry, parse_groups
 
 
-def load_log(file, format, groups):
+def load_log(file, log_format, groups, parsers=None):
+    if parsers is None:
+        parsers = {}
     with open(file) as logF:
-        return {Entry(**parse_entry(line, format, groups)) for line in logF}
+        entries = [parse_entry(line, log_format, groups) for line in logF]
+        return {Entry(**parse_groups(e, parsers)) for e in entries}
--- test/test_logalytics/engine/test_engine.py
+++ test/test_logalytics/engine/test_engine.py
@@ -1,5 +1,6 @@
 import unittest
 from pathlib import Path
+from datetime import datetime
 
 from logalytics.engine.engine import load_log
 from logalytics.model.entry import Entry
@@ -10,8 +11,10 @@ log_file = Path(__file__).parent.parent.parent.parent / "sim" / "data" / "primes
 
 class TestEntry(unittest.TestCase):
     def test_load_log(self):
-        # 2022-10-20 21:16:54,057: Testing: 933559
         log_format = "(....-..-.. ..:..:..,...): (.*): (.*)"
         log_groups = [TIMESTAMP, MODULE, MESSAGE]
-        entries = load_log(log_file, log_format, log_groups)
+        date_format = "%Y-%m-%d %H:%M:%S,%f"
+        parsers = {TIMESTAMP: lambda x: datetime.strptime(x, date_format)}
+        entries = load_log(log_file, log_format, log_groups, parsers)
         self.assertTrue(all(isinstance(x, Entry) for x in entries))
+        self.assertTrue(all(isinstance(x.timestamp, datetime) for x in entries))
